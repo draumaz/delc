6f2f8a657d95333fb92b17a0693001e6811df131
draumaz <77967471+draumaz@users.noreply.github.com>
2022-11-01 15:45:02 -0700

    Update delc.sh

--- delc.sh
+++ delc.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
 case "" in
-  ${PRIMARY_KEYMAP}) echo "missing primary keymap config" && CFG_CHK=1 ;;
-  ${SECONDARY_KEYMAP}) echo "missing secondary keymap config" && CFG_CHK=1 ;;
+  ${PRIMARY}) echo "missing primary keymap config" && CFG_CHK=1 ;;
+  ${SECONDARY}) echo "missing secondary keymap config" && CFG_CHK=1 ;;
 esac; case ${CFG_CHK} in 1) exit ;; esac
 
-FILE="/tmp/delc.conf" # generate file to monitor layout
+FILE="/tmp/delc.conf"
 touch $FILE
 
 case `cat $FILE` in
   "")
-    setxkbmap ${PRIMARY_KEYMAP}
+    setxkbmap ${PRIMARY}
     echo 0 > $FILE
-    printf "initialized file at ${FILE}.\nswitched to primary keymap (${PRIMARY_KEYMAP}).\n"
+    printf "initialized file at ${FILE}.\nswitched to primary keymap (${PRIMARY}).\n"
     ;;
   "0")
-    setxkbmap ${SECONDARY_KEYMAP}
+    setxkbmap ${SECONDARY}
     echo 1 > $FILE
-    printf "switched to secondary keymap (${SECONDARY_KEYMAP}).\n"
+    printf "switched to secondary keymap (${SECONDARY}).\n"
     ;;
 
   "1")
-    setxkbmap ${PRIMARY_KEYMAP}
+    setxkbmap ${PRIMARY}
     echo 0 > $FILE
-    printf "switched to primary keymap (${PRIMARY_KEYMAP}).\n"
+    printf "switched to primary keymap (${PRIMARY}).\n"
     ;;
 esac
